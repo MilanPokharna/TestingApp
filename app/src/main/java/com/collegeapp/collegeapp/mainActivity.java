12e07715e305d1d4a621461c9ac0ace15de78b41
MiLaN <milan.pokharna1998@gmail.com>
2018-06-05 16:39:41 +0530

    Initial commit

---
+++ app/src/main/java/com/collegeapp/collegeapp/mainActivity.java
@@ -0,0 +1,23 @@
+package com.collegeapp.collegeapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainActivity extends AppCompatActivity {
+
+    public ViewPager mviewpager;
+    public TabLayout mTabLayout;
+    public sectionAdapter msectionAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mviewpager = findViewById(R.id.pager);
+        mTabLayout = findViewById(R.id.tablayout);
+        msectionAdapter = new sectionAdapter(getSupportFragmentManager());
+        mviewpager.setAdapter(msectionAdapter);
+        mTabLayout.setupWithViewPager(mviewpager);
+    }
+}
