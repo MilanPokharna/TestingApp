8bc11e07e2616d5492ef7791683bd04a8233e2f9
MiLaN <milan.pokharna1998@gmail.com>
2018-07-02 01:18:38 +0530

    new Commit

--- app/src/main/java/com/collegeapp/collegeapp/activities/scrollActivity.java
+++ app/src/main/java/com/collegeapp/collegeapp/activities/scrollActivity.java
@@ -4,15 +4,14 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.collegeapp.collegeapp.R;
 import com.collegeapp.collegeapp.adapters.DisplayAdaptor;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,54 +22,51 @@ import com.google.firebase.database.ValueEventListener;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-    public class scrollActivity extends AppCompatActivity {
+public class scrollActivity extends AppCompatActivity {
 
-        ProgressDialog progressDialog;
-        DatabaseReference myref;
-        public String key;
-        public  DisplayAdaptor displayAdaptor;
-        public ViewPager mViewpager;
-        TabLayout mtablayout;
+    ProgressDialog progressDialog;
+    DatabaseReference myref;
+    public String key;
+    public  DisplayAdaptor displayAdaptor;
+    public ViewPager mViewpager;
 
-        public CollapsingToolbarLayout collapsingToolbarLayout;
-        @BindView(com.collegeapp.collegeapp.R.id.toolbar)
-        Toolbar toolbar;
-        @BindView(com.collegeapp.collegeapp.R.id.profileimage)
-        ImageView profileimage;
+    public CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(com.collegeapp.collegeapp.R.id.tool_bar)
+    Toolbar toolbar;
+    @BindView(R.id.profileImg)
+    ImageView profileimage;
+    private boolean mShowingFragments = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.collegeapp.collegeapp.R.layout.activity_scrolling);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        mViewpager =(ViewPager)findViewById( R.id.viewPager);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        Intent i = getIntent();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(com.collegeapp.collegeapp.R.layout.activity_scrolling);
-            ButterKnife.bind(this);
-            setSupportActionBar(toolbar);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            mViewpager =(ViewPager)findViewById(com.collegeapp.collegeapp.R.id.Vpager);
-            mtablayout =(TabLayout)findViewById(com.collegeapp.collegeapp.R.id.tablayout_);
-            Intent i = getIntent();
+        key = i.getStringExtra("key");
+        myref = FirebaseDatabase.getInstance().getReference().child("root").child("contact list").child("chairpersons");
 
-            key = i.getStringExtra("key");
-            myref = FirebaseDatabase.getInstance().getReference().child("root").child("contact list").child("chairpersons");
+        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String a = dataSnapshot.child(key).child("image").getValue().toString();
+                Glide.with(getApplicationContext()).load(a).into(profileimage);
+            }
 
-            myref.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    String a = dataSnapshot.child(key).child("image").getValue().toString();
-                    Glide.with(getApplicationContext()).load(a).into(profileimage);
-                }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
-                }
-            });
-
-            displayAdaptor = new DisplayAdaptor(getSupportFragmentManager(), key);
-            mViewpager.setAdapter(displayAdaptor);
-            mtablayout.setupWithViewPager(mViewpager);
+        displayAdaptor = new DisplayAdaptor(getSupportFragmentManager(), key);
+        mViewpager.setAdapter(displayAdaptor);
 
 
-        }
     }
+}
 
