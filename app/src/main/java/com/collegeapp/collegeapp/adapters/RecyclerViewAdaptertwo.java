b5742cdad4a9230fc003cac224c201303509dec5
MiLaN <milan.pokharna1998@gmail.com>
2018-07-15 23:39:34 +0530

    latest commit

--- app/src/main/java/com/collegeapp/collegeapp/adapters/RecyclerViewAdaptertwo.java
+++ app/src/main/java/com/collegeapp/collegeapp/adapters/RecyclerViewAdaptertwo.java
@@ -129,7 +129,7 @@ public class RecyclerViewAdaptertwo extends RecyclerView.Adapter<RecyclerViewAda
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
                 connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            //we are connected to a network
+
             return true;
         } else
             return false;
